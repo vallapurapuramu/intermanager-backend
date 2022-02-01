9fb66fb2046f4b380914a73d9e4a9c25bef9220d
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2022-01-31 19:25:32 -0600

    added home api for student controller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -7,10 +7,17 @@ const Application = use("App/Models/Application");
 const Comment = use("App/Models/Comments");
 const User = use("App/Models/User");
 const helpers = use("Helpers");
-const { validate } = use("Validator");
 const env = use("Env");
 
 class StudentController {
+
+    async home({ request, response, auth }) {
+        return response.status(200).json({
+          status: 200,
+          message: "user home screen",
+        });
+    }
+      
 }
 
 module.exports = StudentController
