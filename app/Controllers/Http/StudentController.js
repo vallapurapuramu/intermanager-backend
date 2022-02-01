68bad2156c061e7b124b27c527445cb71db0ca50
s541910 <s541910@nwmissouri.edu>
2022-01-31 19:14:55 -0600

    added required fields in controller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,4 +1,14 @@
 'use strict'
+const Major = use("App/Models/Major");
+const StudentDetails = use("App/Models/StudentDetails");
+const Database = use("Database");
+const Internship = use("App/Models/Internship");
+const Application = use("App/Models/Application");
+const Comment = use("App/Models/Comments");
+const User = use("App/Models/User");
+const helpers = use("Helpers");
+const { validate } = use("Validator");
+const env = use("Env");
 
 class StudentController {
 }
